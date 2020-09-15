ad1244b5de138861c15c9d1e6c44d36519b32874
lyexuan <414741043@qq.com>
2020-09-15 11:28:09 +0800

    add less translation

--- posts/draft/resume.md.js
+++ posts/draft/resume.md.js
@@ -1,1 +1,1 @@
-webpackJsonp([17],{559:function(t,e){t.exports={content:["article"],meta:{title:"\u7b80\u5386",createDate:"2020-03-07T00:00:00.000Z",publishDate:"2020-03-07T00:00:00.000Z",category:"draft",abstract:"\u7b80\u5386",filename:"posts/draft/resume.md"}}}});
+webpackJsonp([24],{559:function(t,e){t.exports={content:["article"],meta:{title:"\u7b80\u5386",createDate:"2020-03-07T00:00:00.000Z",publishDate:"2020-03-07T00:00:00.000Z",category:"draft",abstract:"\u7b80\u5386",filename:"posts/draft/resume.md"}}}});
